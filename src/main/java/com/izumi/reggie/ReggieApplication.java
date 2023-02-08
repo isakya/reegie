2936b16aef024fa90d5ff9b8cf29fdd6ab524eb0
konglong <1016361268@qq.com>
2023-02-08 18:17:07 +0800

    完善登陆功能

--- src/main/java/com/izumi/reggie/ReggieApplication.java
+++ src/main/java/com/izumi/reggie/ReggieApplication.java
@@ -3,9 +3,12 @@ package com.izumi.reggie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class, args);
