cb9fcbab6c1f5dd19019c635de1f0e5c1b18393e
konglong <1016361268@qq.com>
2023-02-11 05:55:33 +0800

    登陆校验

--- src/main/java/com/izumi/reggie/controller/UserController.java
+++ src/main/java/com/izumi/reggie/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.izumi.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.izumi.reggie.common.R;
 import com.izumi.reggie.entity.User;
 import com.izumi.reggie.service.UserService;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -43,4 +45,36 @@ public class UserController {
         return R.error("短信发送失败");
     }
 
+    @PostMapping("/login")
+    public R<User> login(@RequestBody Map map, HttpSession session){
+        log.info(map.toString());
+        // 获取手机号
+        String phone = map.get("phone").toString();
+
+        // 获取验证码
+        String code = map.get("code").toString();
+        // 从Session中获取保存的验证码
+        Object codeInSession = session.getAttribute(phone);
+        // 进行验证码比对（页面提交的验证码和Session中保存的验证码比对）
+        if(codeInSession != null && codeInSession.equals(code)){
+            //如果能够比对成功，说明登录成功
+
+            LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(User::getPhone,phone);
+
+            User user = userService.getOne(queryWrapper);
+            if(user == null){
+                //判断当前手机号对应的用户是否为新用户，如果是新用户就自动完成注册
+                user = new User();
+                user.setPhone(phone);
+                user.setStatus(1);
+                userService.save(user);
+            }
+            session.setAttribute("user",user.getId());
+            return R.success(user);
+        }
+
+
+        return R.error("");
+    }
 }
--- target/classes/backend/api/login.js
+++ target/classes/backend/api/login.js
@@ -12,3 +12,5 @@ function logoutApi(){
     'method': 'post',
   })
 }
+
+
