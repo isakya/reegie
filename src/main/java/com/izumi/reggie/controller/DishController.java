66d36a1614780d8ba2659da6b0036e9aca8a8005
konglong <1016361268@qq.com>
2023-02-10 18:48:34 +0800

    菜品详情、修改菜品

--- src/main/java/com/izumi/reggie/controller/DishController.java
+++ src/main/java/com/izumi/reggie/controller/DishController.java
@@ -75,4 +75,20 @@ public class DishController {
         dishDtoPage.setRecords(list);
         return R.success(dishDtoPage);
     }
+
+    // 根据id查询菜品信息和对应的口味信息
+    @GetMapping("/{id}")
+    public R<DishDto> get(@PathVariable Long id) {
+        DishDto dishDto = dishService.getByIdWithFlavor(id);
+        return R.success(dishDto);
+    }
+
+    // 修改菜品
+    @PutMapping
+    public R<String> update(@RequestBody DishDto dishDto) {
+        log.info(dishDto.toString());
+        dishService.updateWithFlavor(dishDto);
+        return R.success("更新菜品成功");
+    }
+
 }
